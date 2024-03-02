ffd65e8ae56c2752258ec1f01cc08e9dc2d6931e
maplesar <maplesar@student.21-school.ru>
2024-03-02 20:15:06 +0300

    version catalog

--- newsapi/build.gradle.kts
+++ newsapi/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.kotlinSerialization) apply false
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
 }
